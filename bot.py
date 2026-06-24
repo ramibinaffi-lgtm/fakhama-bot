5ede8cd8738e1c33f49a8d48c335a4b8e0291d3a
ramibinaffi-lgtm <ramibinaffi@gmail.com>
2026-06-24 20:59:54 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,17 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
 import telebot
 from telebot import types
 import requests
